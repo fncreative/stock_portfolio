7737bbcc12d42319b5692144f3b8a43a3424e3bd
Daniel Frey <dann.frey@me.com>
2018-12-06 14:39:52 -0800

    updated from today

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,7 +1,9 @@
 from . import app
 from flask import render_template
 
+
 @app.errorhandler(404)
 def not_found(error):
-    """404 Error Handler"""
+    """Custom 404 Not Found handler.
+    """
     return render_template('404_notfound.html', error=error), 404
---
+++ src/routes.py
@@ -0,0 +1,59 @@
+from flask import render_template, redirect, url_for, abort
+from .forms import CompanySearchForm
+from .models import Company, db
+import requests as req
+from . import app
+import json
+
+
+@app.route('/')
+def home():
+    """
+    """
+    return render_template('home.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def company_search():
+    """
+    """
+    form = CompanySearchForm()
+
+    if form.validate_on_submit():
+        res = req.get(f'https://api.iextrading.com/1.0/stock/{ form.data["symbol"] }/company')
+
+        try:
+            data = json.loads(res.text)
+            company = {
+                'symbol': data['symbol'],
+                'companyName': data['companyName'],
+                'exchange': data['exchange'],
+                'industry': data['industry'],
+                'website': data['website'],
+                'description': data['description'],
+                'CEO': data['CEO'],
+                'issueType': data['issueType'],
+                'sector': data['sector'],
+            }
+
+            new_company = Company(**company)
+            # Company(
+            #     symbol=data['symbol'],
+            #     ...
+            # )
+            db.session.add(new_company)
+            db.session.commit()
+
+            return redirect(url_for('.portfolio_detail'))
+
+        except json.JSONDecodeError:
+            abort(404)
+
+    return render_template('portfolio/search.html', form=form)
+
+
+@app.route('/portfolio')
+def portfolio_detail():
+    """
+    """
+    return render_template('portfolio/portfolio.html')
